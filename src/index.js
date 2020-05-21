40b6745ec4514c724c747884884416d41ed73694
vardhman <vardhmanhundia_mahesh@srmuniv.edu.in>
2020-05-21 19:45:46 +0530

    updated\

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
 import { createLogger } from 'redux-logger';
 import 'tachyons';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware  } from 'redux';
 import { searchRobots } from './reducer';
+import * as serviceWorker from './serviceWorker';
 
 const logger = createLogger();
 const store = createStore(searchRobots, applyMiddleware(logger));
@@ -16,4 +16,5 @@ ReactDOM.render(
     <Provider store={store}>
         <App />
     </Provider>, document.getElementById('root'));
-registerServiceWorker();
+
+serviceWorker.default();
